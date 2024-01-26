29b712f97379dab246f5c184b2010b8704b15a6c
AlvinOt <thisisalvinhere@gmail.com>
2024-01-26 20:50:16 +0000

    usr auth serv

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""sql alchemy user model"""
+
+from typing import Optional
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User class"""
+    __tablename__ = 'users'
+
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: Optional[str] = Column(String(250), nullable=True)
+    reset_token: Optional[str] = Column(String(250), nullable=True)
+
+    def __repr__(self) -> str:
+        """Returns a string representation of the User object"""
+        return f"User: id={self.id}"
